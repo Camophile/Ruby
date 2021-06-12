353dd700a5e8b68dce5fc96b7a06bc3c4f861762
arman <kas1er@hotmail.com>
2021-06-12 14:12:33 -0700

    able to get entrie, evaluate whether they are num or not and output max if they are only 2 entries

--- Module1/problems/max_number.rb
+++ Module1/problems/max_number.rb
@@ -1,17 +1,31 @@
 puts "enter two numbers"
 
-numbersArray = []
+while true
+  numbersArray = []
+  numbers = gets.chomp
 
-numbers = gets.chomp
+  numbers.split(" ").each do |n|
+    num = Float(n) rescue false
+    numbersArray.push(num)
+  end
 
-numbers.split(" ").each do |n|
-  numbersArray.push(n)
+  if numbersArray.length == 2 # only calls when there are 2 entries
+    first = numbersArray[0]
+    second = numbersArray[1]
+      
+    if first != false && second != false
+      if first == second
+        puts "#{first.to_i} and #{second.to_i} are the same number. Try again!"
+      else
+        max = first > second ? first : second  
+        puts "Max: #{max.to_i}"
+        break
+      end
+    else
+      puts "please enter a valid number"
+    end
+  else 
+    puts "please enter exactly two numbers"
+  end
 end
-
-if numbersArray.length > 2
-  puts "please only enter two numbers"
-else
-  puts "you have #{numbersArray.length} numbers"
-end
-
 
