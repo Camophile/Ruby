7be02f5a8bf196ac3c7f3ad4e6b16b6267bdecc8
arman <kas1er@hotmail.com>
2021-06-12 12:51:55 -0700

    add comments

--- Module1/game.rb
+++ Module1/game.rb
@@ -1,34 +1,33 @@
 puts "Welcome to guess the number"
-#print "What's your name? "
-# name = gets.chomp
-#puts "welcome #{name}"
+print "What's your name? "
+name = gets.chomp
+puts "welcome #{name}"
 
 puts "compiling random number generator..."
 
-target = rand(100) + 1
-target = 10
-guesses = 10
+target = rand(100) + 1 # gives the random number we want user to guess
+guesses = 10 # counts down from number of guesses
 
 puts "You have #{guesses} tries to guess the correct number"
 
-while guesses > 0
+while guesses > 0 # repeat loop until they have 0 tries
   
   guess = gets.chomp.to_i
-  guesses -=1
+  guesses -=1 # increments guesses count down after the new round is initiated
 
-  s = guesses > 1 ? "es" : ""
+  s = guesses > 1 ? "es" : "" # for the plural statement if they fail
   
   if guess == target 
-    if guesses <= 1
+    if guesses <= 1 # reverses plural statement since we're counting down (could be improved)
       puts "inside if: ${guesses}"
       s = "es"
     elsif guesses == 9
       s = ""
     end
 
-    puts "outside if: #{guesses}"
     puts "#{guess} is correct!\nyou guessed it in #{10 - guesses} guess#{s}"
-    break 
+
+    break # break out of loop 
 
   elsif guesses >= 1
       if guess > target
