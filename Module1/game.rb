7d09204982df0149a202f11f29b7148ec3be7054
arman <kas1er@hotmail.com>
2021-06-12 12:37:21 -0700

    added guess tracker so we tell the user how many guesses they have left + added logic for plural statements

--- Module1/game.rb
+++ Module1/game.rb
@@ -1,11 +1,12 @@
 puts "Welcome to guess the number"
-print "What's your name? "
-name = gets.chomp
-puts "welcome #{name}"
+#print "What's your name? "
+# name = gets.chomp
+#puts "welcome #{name}"
 
 puts "compiling random number generator..."
 
 target = rand(100) + 1
+target = 10
 guesses = 10
 
 puts "You have #{guesses} tries to guess the correct number"
@@ -15,9 +16,18 @@ while guesses > 0
   guess = gets.chomp.to_i
   guesses -=1
 
+  s = guesses > 1 ? "es" : ""
+  
   if guess == target 
+    if guesses <= 1
+      puts "inside if: ${guesses}"
+      s = "es"
+    elsif guesses == 9
+      s = ""
+    end
 
-    puts "#{guess} is correct!"
+    puts "outside if: #{guesses}"
+    puts "#{guess} is correct!\nyou guessed it in #{10 - guesses} guess#{s}"
     break 
 
   elsif guesses >= 1
@@ -26,7 +36,7 @@ while guesses > 0
       else
         puts "#{guess} is too low"
       end
-    puts "#{guesses} guesses left\nguess again."
+    puts "#{guesses} guess#{s} left\nguess again."
   else 
     puts "You're out of guesses. GOODBYE!"
     puts "correct number is #{target}"
