86381236dd05aed7755b0c76c579ce21561d3ebc
arman <kas1er@hotmail.com>
2021-06-11 17:18:04 -0700

    able to randomize number and provide flow to guess the number until fails

--- Module1/game.rb
+++ Module1/game.rb
@@ -4,5 +4,27 @@ name = gets.chomp
 puts "welcome #{name}"
 
 puts "compiling random number generator..."
-target = rand(100)
-puts target
+
+target = rand(100) + 1
+guesses = 10
+
+puts "You have #{guesses} tries to guess the correct number"
+
+while guesses > 0
+  
+  guess = gets.chomp
+  guesses -=1
+
+  if guess == target 
+
+    puts "#{guess} is correct!"
+    break 
+
+  elsif guesses >= 1
+    puts "#{guess} is incorrect\nyou have #{guesses} guesses left\nguess again."
+  else 
+    puts "You're out of guesses. GOODBYE!"
+    puts "correct number is #{target}"
+
+  end
+end
