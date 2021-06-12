d702d8253bc487f9df6f3e6ceb4d12b324614ad9
arman <kas1er@hotmail.com>
2021-06-12 14:18:59 -0700

    updated comments and too out to_i evaluator for floats

--- Module1/problems/max_number.rb
+++ Module1/problems/max_number.rb
@@ -13,19 +13,19 @@ while true
     first = numbersArray[0]
     second = numbersArray[1]
       
-    if first != false && second != false
-      if first == second
-        puts "#{first.to_i} and #{second.to_i} are the same number. Try again!"
-      else
-        max = first > second ? first : second  
-        puts "Max: #{max.to_i}"
-        break
+    if first != false && second != false # if either entry is not a number will ask them to enter again
+      if first == second # if they're the same number, asks them to try again
+        puts "#{first} and #{second} are the same number. Try again!"
+      else 
+        max = first > second ? first : second # evaluates the larger of the two and assigns to `max`  
+        puts "Max: #{max}"
+        break # breaks out of the loop after evaluating larger number
       end
     else
-      puts "please enter a valid number"
+      puts "please enter a valid number" # if either first or second aren't a number
     end
   else 
-    puts "please enter exactly two numbers"
+    puts "please enter exactly two numbers" # if there is more than 2 entries
   end
 end
 
