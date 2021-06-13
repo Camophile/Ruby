5d7de65caa608239ef5871dad1ad70659873801f
arman <kas1er@hotmail.com>
2021-06-13 14:14:17 -0700

    able to loop through number input and give current number + index of loop

--- Module1/problems/rearrange.rb
+++ Module1/problems/rearrange.rb
@@ -1,8 +1,25 @@
 while true
   print "Enter a number: "
   entry = gets.chomp
-  num = Integer(entry) rescue false
-  if num
+  number = Integer(entry) rescue false
+  if number
+    sortedArray = [] # final array to be printed
+
+    currentNum = 0 #current number being compared in the loop
+    lastNum = 0 # last assessed num, which is given num at the end of loop
+
+    numberArray = entry.split("")
+    numberArray.each_with_index do | num, i |
+      #if i >= i # if we're after the first loop
+       # if sortedArray[i] > sortedArray[i-1]
+        #end  
+      #else # first iteration always puts number at beginning
+        #sortedArray[i] = num
+        #sortedArray.unshift(num)
+      #end # if inside loop 
+      puts "num: #{num}, i: #{i}"
+
+    end # each 
     break
-  end
-end
+  end # if
+end # while 
