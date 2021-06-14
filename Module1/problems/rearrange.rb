95127f375bbe440e46d7239c9e362faa5273ddbf
arman <kas1er@hotmail.com>
2021-06-14 10:23:43 -0700

    put the switch into a function def and add switched to keep track of mvmnt

--- Module1/problems/rearrange.rb
+++ Module1/problems/rearrange.rb
@@ -1,27 +1,28 @@
-while true
-  # print "Enter a number: "
-  entry = "527938" # gets.chomp
-  number = Integer(entry) rescue false
+def sortIndex(array)
+  switch = false # sets switch to false at each new iteration to check for any movement
+  length = array.length
+  length.times do | i |
+    backwardsIndex = length - i # start at the end of the array and work backwards
+    unless backwardsIndex == length # ignore when backwardsIndex is == length so we don't attempt to look outside of possible elements
+      if array[backwardsIndex] > array[backwardsIndex - 1] # check if current element is greater than leftmost element
+        array[backwardsIndex], array[backwardsIndex - 1] = array[backwardsIndex - 1], array[backwardsIndex] # if so switch
+        switch = true
+          if switch # if switch is true, there has been movement, so we need to recheck the previous indexes, so call sortIndex again until always returns false, ie no movement
+            sortIndex(array)
+          end # if
+      end #if
+    end # unless
+  end # times
+  array # return the final sorted array
+end
 
+while true
+  print "Enter a number: "
+  entry = gets.chomp
+  number = Integer(entry) rescue false # if not an integer, returns false so will loop back to beginning
   if number
     numberArray = entry.split("")
-    length = numberArray.length
-    puts "numberArray: #{numberArray}"
-    length.times do | i |
-      backwardsIndex = length - i
-      puts "backwardsIndex: #{backwardsIndex}"
-      unless backwardsIndex == length
-        puts "backwardsIndex: #{backwardsIndex}, numberArray[backwardsIndex]: #{numberArray[backwardsIndex]}"
-   
-        if numberArray[backwardsIndex] > numberArray[backwardsIndex - 1]
-
-          numberArray[backwardsIndex], numberArray[backwardsIndex - 1] = numberArray[backwardsIndex - 1], numberArray[backwardsIndex]
-          puts "numberArray after sort: #{numberArray}"
-        end #if
-      end # unless
-
-    end # each 
-    puts "final: #{numberArray}"
+    puts "#{sortIndex(numberArray).join}"
     break
   end # if
 end # while 
