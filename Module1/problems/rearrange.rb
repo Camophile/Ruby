3a72324369e4cbce35b47917b6910f2843ded0c1
arman <kas1er@hotmail.com>
2021-06-15 23:05:21 -0700

    is able to see if it's negative array and sort biggest > smallest or smallest to biggest accordingly, but doesn't account for 0 is negatives

--- Module1/problems/rearrange.rb
+++ Module1/problems/rearrange.rb
@@ -2,36 +2,38 @@ def sortIndex(array)
   switch = false # sets switch to false at each new iteration to check for any movement
   length = array.length
   length.times do | i |
-    index = length - 1 # start at the end of the array and work backwards
+    index = length - i # start at the end of the array and work backwards
     operator = "-"
-    if array[i] == "-" # means it's a negative, so start at beginning of array
+    if array[0] == "-" # means it's a negative, so start at beginning of array
       index = i
       operator = "+"
     end
 
     unless index == length # ignore when backwardsIndex is == length so we don't attempt to look outside of possible elements, ie. when i==0
-#      operator = array[0] == "-" ? "+" : "-"
-puts "operator: #{operator}"
       getElement = index.public_send(operator, 1)
-puts "getElement: #{getElement}"
-      if array[index] > array[getElement] # check if current element is greater than leftmost element
-        array[index], array[getElement] = array[getElement], array[index] # if so switch
-        switch = true
+      unless getElement >= length
+        if array[index] > array[getElement] # check if current element is greater than leftmost element
+          array[index], array[getElement] = array[getElement], array[index] # if so switch
+          switch = true
           if switch # if switch is true, there has been movement, so we need to recheck the previous indexes, so call sortIndex again until always returns false, ie no movement
             sortIndex(array)
           end # if
-      end #if
+#        elsif array[index] == 0 && operator == "+"
+#          puts "in elsif - array[index]: #{array[index]}, array[2]: #{array[2]}"
+#          array[index], array[2] = array[2], array[index]
+        end #if
+      end #unless
+puts "array: #{array}"
     end # unless
   end # times
   array # return the final sorted array
 end
 
 while true
-  # print "Enter a number: "
-  entry = "-12532" # gets.chomp
+  print "Enter a number: "
+  entry = gets.chomp
   number = Integer(entry) rescue false # if not an integer, returns false so will loop back to beginning
   if number
-#    puts "in number IF: #{number}"
     numberArray = entry.split("")
     puts "#{sortIndex(numberArray).join}"
     break
