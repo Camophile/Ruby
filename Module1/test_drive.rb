1f97ececbb5ad1405df37939c74f07fe103679dc
arman <kas1er@hotmail.com>
2021-06-15 20:44:14 -0700

    added a mileage method that returns mileage/fuel used

--- Module1/test_drive.rb
+++ Module1/test_drive.rb
@@ -8,11 +8,19 @@ def sound_horn
   puts "honk honk"
 end
 
-def use_headlights(brightness)
+def use_headlights(brightness="blinding")
   puts "turning headlights to #{brightness}"
   puts "run 'em over"
 end
 
+def mileage(miles, fuel)
+  miles/fuel
+end
+
 sound_horn
 accelerate
-use_headlights("fucking bright")
+use_headlights
+puts "you got #{mileage(400, 12)} MPG on this trip"
+puts "This car averages #{mileage(11432, 366)} MPG"
+
+
