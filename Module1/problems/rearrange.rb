56d7ac82b86990c553cdca69348786c24a62679f
arman <kas1er@hotmail.com>
2021-06-17 23:39:22 -0700

    sorting array but 0 is still showing up at the front negatives

--- Module1/problems/rearrange.rb
+++ Module1/problems/rearrange.rb
@@ -10,28 +10,27 @@ def sortIndex(array)
     end
 
     unless index == length # ignore when backwardsIndex is == length so we don't attempt to look outside of possible elements, ie. when i==0
-      getElement = index.public_send(operator, 1)
+      getElement = index.public_send(operator, 1) # becomes either `index + 1` or `index - 1`
       unless getElement >= length
         if array[index] > array[getElement] # check if current element is greater than leftmost element
           array[index], array[getElement] = array[getElement], array[index] # if so switch
           switch = true
           if switch # if switch is true, there has been movement, so we need to recheck the previous indexes, so call sortIndex again until always returns false, ie no movement
-            sortIndex(array)
+          sortIndex(array)
           end # if
-#        elsif array[index] == 0 && operator == "+"
-#          puts "in elsif - array[index]: #{array[index]}, array[2]: #{array[2]}"
-#          array[index], array[2] = array[2], array[index]
+        # elsif array[index] == 0 && operator == "+"
+        #  puts "in elsif - array[index]: #{array[index]}, array[2]: #{array[2]}"
+        #  array[index], array[2] = array[2], array[index]
         end #if
       end #unless
-puts "array: #{array}"
     end # unless
   end # times
   array # return the final sorted array
 end
 
 while true
-  print "Enter a number: "
-  entry = gets.chomp
+  # print "Enter a number: "
+  entry = "561029402948274" # gets.chomp
   number = Integer(entry) rescue false # if not an integer, returns false so will loop back to beginning
   if number
     numberArray = entry.split("")
