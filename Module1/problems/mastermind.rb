1cc9b68acb40102a9d3208f842f66216a1a3de49
arman <kas1er@hotmail.com>
2021-06-24 15:02:37 -0700

    able to loop through guess and reset if we don't present an exact match

--- Module1/problems/mastermind.rb
+++ Module1/problems/mastermind.rb
@@ -1,19 +1,22 @@
-# code = Array.new(4) { rand(1..6) } 
-code = [2, 6 ,3 , 4]
-puts "code: #{code}"
-# guess = gets.chomp.split("").to_i;
-guess = [3, 6, 5, 2]
-puts "guess: #{guess}"
-
 hint = String.new
 
-code.each_with_index do | value, i |
-  if value == guess[i]
-    hint.prepend("X")
-  elsif code.include? guess[i]
-    hint.concat("x")
-  end
-  puts "current value: #{value}, current guess: #{guess[i]}, hint: #{hint}"
-end 
+until hint == "XXXX"
+        hint = ""
+	# code = Array.new(4) { rand(1..6) } 
+	code = [2, 6 ,3 , 4]
+	puts "code: #{code}"
+	guess = gets.chomp.split("").map(&:to_i)
+	# guess = [3, 6, 5, 2]
 
+	puts "guess: #{guess}"
 
+        code.each_with_index do | value, i |
+          if value == guess[i]
+            hint.prepend("X")
+          elsif code.include? guess[i]
+            hint.concat("x")
+          end
+          puts "current value: #{value}, current guess: #{guess[i]}, hint: #{hint}"
+        end
+
+end
