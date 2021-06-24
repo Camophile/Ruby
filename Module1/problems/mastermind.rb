a9953cbebb10d8557978ff0809c75439dcfb940f
arman <kas1er@hotmail.com>
2021-06-24 14:03:44 -0700

    able to sort through the code and the guess and output the hit correctly

--- Module1/problems/mastermind.rb
+++ Module1/problems/mastermind.rb
@@ -1,3 +1,19 @@
 # code = Array.new(4) { rand(1..6) } 
-code = [1, 2 ,3 , 4]
+code = [2, 6 ,3 , 4]
 puts "code: #{code}"
+# guess = gets.chomp.split("").to_i;
+guess = [3, 6, 5, 2]
+puts "guess: #{guess}"
+
+hint = String.new
+
+code.each_with_index do | value, i |
+  if value == guess[i]
+    hint.prepend("X")
+  elsif code.include? guess[i]
+    hint.concat("x")
+  end
+  puts "current value: #{value}, current guess: #{guess[i]}, hint: #{hint}"
+end 
+
+
