5d6757db298c5fdf6daf5852685d3cc783a57aba
arman <kas1er@hotmail.com>
2021-06-24 15:40:34 -0700

    able to check format of entry and only continue play if 4 digit number entered in valid format

--- Module1/problems/mastermind.rb
+++ Module1/problems/mastermind.rb
@@ -6,12 +6,14 @@ guessCount = 0
 until hint == "XXXX"
         hint = ""
 	# code = Array.new(4) { rand(1..6) } 
-	code = [2, 6 ,3 , 4]
+	code = [1, 2 ,3 , 4]
 	puts "code: #{code}"
         print "[#{guessCount + 1}] Enter your guess: "
-	guess = gets.chomp.split("").map(&:to_i)
+        entry = gets.chomp
+        puts "entry: #{entry}, size: #{entry.size}"
 	# guess = [3, 6, 5, 2]
-
+  if entry.size == 4 && entry.to_i > 0
+	guess = entry.split("").map(&:to_i)
 	puts "guess: #{guess}"
 
         code.each_with_index do | value, i |
@@ -21,8 +23,10 @@ until hint == "XXXX"
             hint.concat("x")
           end
           puts "current value: #{value}, current guess: #{guess[i]}, hint: #{hint}"
-        end
-guessCount += 1
+        end # each
+    guessCount += 1
+  end # if
 puts "guessCount: #{guessCount}"
 puts ""
-end
+end # until
+puts "Nice work! You took #{guessCount} guess#{guessCount == 1 ? "" : "es"}"
