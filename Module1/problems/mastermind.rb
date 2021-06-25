cced436660d0d2b4ac6f1060299112bc7d5070f6
arman <kas1er@hotmail.com>
2021-06-24 17:01:43 -0700

    able to check the > 7 limit and skips the input when added. Also refactored how guessCount is incremented

--- Module1/problems/mastermind.rb
+++ Module1/problems/mastermind.rb
@@ -1,28 +1,32 @@
 hint = String.new
 puts "MASTERMIND"
-
-guessCount = 0
+guessCount = 1
 
 until hint == "XXXX"
-        unless hint == "" 
-          puts hint
-        end 
-        hint = ""
+
+        unless hint == "" # doesn't show hint if none of the digits match 
+          puts hint 
+        end
+
+        hint = "" # reset the hint on each new attempt
 	# code = Array.new(4) { rand(1..6) } 
 	code = [1, 2 ,3 , 4]
-        print "[#{guessCount + 1}] Enter your guess: "
+        print "[#{guessCount}] Enter your guess: "
         entry = gets.chomp
-  if entry.size == 4 && entry.to_i > 0
-	guess = entry.split("").map(&:to_i)
-
-        code.each_with_index do | value, i |
-          if value == guess[i]
-            hint.prepend("X")
-          elsif code.include? guess[i]
-            hint.concat("x")
-          end
-        end # each
-    guessCount += 1
+  if entry.size == 4 && entry.to_i > 0 # check if the string entered is 4 long and numerical/not negative
+    guess = entry.split("").map(&:to_i)
+    unless guess.any? { | v | v >= 7 } # checks each element and if any >= 7 skips
+    code.each_with_index do | value, i |
+      if value == guess[i]
+        hint.prepend("X")
+      elsif code.include? guess[i]
+        hint.concat("x") # need to refactor this as it provide 'x'' for each guess of a value and doesn't take into consideration how many times a value occurs in the code
+      end
+    end # each
+    guessCount = hint == "XXXX" ? guessCount : guessCount + 1 # either a correct guess or an entry has a digit > 7, either way count is paused
+    else # detected a digit > 6 so guess not counted
+      hint = ""
+    end # unless
   end # if
 end # until
 puts "Nice work! You took #{guessCount} guess#{guessCount == 1 ? "" : "es"}"
