aaeea960c9414b528ba5f346148342df3618a012
arman <kas1er@hotmail.com>
2021-06-24 21:45:58 -0700

    added a hash to keep track of correct indexes and correct placement value to front

--- Module1/problems/mastermind.rb
+++ Module1/problems/mastermind.rb
@@ -1,35 +1,59 @@
-hint = String.new
-puts "MASTERMIND"
+code = Array.new(4) { rand(1..6) } 
+hint = []
 guessCount = 1
 
-until hint == "XXXX"
+puts "MASTERMIND"
 
-        unless hint == "" # doesn't show hint if none of the digits match 
-          puts hint 
+until hint.join("") == "XXXX"
+
+        unless hint == [] # doesn't show hint if none of the digits match 
+          puts hint.join("") 
         end
 
-        hint = "" # reset the hint on each new attempt
-	# code = Array.new(4) { rand(1..6) } 
-	code = [1, 2 ,3 , 4]
+        trackHash = {} # initiate track hash to see previous matches in the round
+        hint = [] # reset the hint on each new attempt
+
+	# code = [1, 2 ,3 , 4]
         print "[#{guessCount}] Enter your guess: "
         entry = gets.chomp
+
   if entry.size == 4 && entry.to_i > 0 # check if the string entered is 4 long and numerical/not negative
     guess = entry.split("").map(&:to_i)
     unless guess.any? { | v | v >= 7 } # checks each element and if any >= 7 skips
-    code.each_with_index do | value, i |
-      if value == guess[i]
-        hint.prepend("X")
-      elsif code.include? guess[i]
-        puts "before delete"
-        puts "i: #{i}, code: #{code}" 
-        code.delete_at(i)
-        hint.concat("x") # need to refactor this as it provide 'x'' for each guess of a value and doesn't take into consideration how many times a value occurs in the code
-        puts "code after delete: #{code}"
-      end # if
-    end # each
-    guessCount = hint == "XXXX" ? guessCount : guessCount + 1 # either a correct guess or an entry has a digit > 7, either way count is paused
-    else # detected a digit > 6 so guess not counted
-      hint = ""
+
+      guess.each_with_index do | guessValue, i |
+        code.each_with_index do | codeValue, y |
+puts "guess index: #{i}, guessValue: #{guessValue}, code index: #{y}, codeValue: #{codeValue}"
+          if guessValue == codeValue
+puts "inside values maching"
+            matchValue = guess[i]
+puts "#{matchValue}"
+            if i == y
+puts "inside indexes matching"
+puts "trackHash: #{trackHash}, y: #{y}"
+              if trackHash[y] == matchValue # we look in the track hash to see if this index was caught when indexes didn't match
+puts "inside if trackHash == y: trackHash[#{y}]: #{trackHash[y]}"
+                hint.pop if hint[-1] == "x"
+                # trackHash.delete(y)
+              end # if
+              hint.unshift("X") # place "X" at beginning when indexes match
+              trackHash[y] = matchValue
+puts "add X to beginning of hint: #{hint}, add value to trackHash: #{trackHash}"
+            else # values match but not indexes
+              unless trackHash[y] == matchValue # we look in the track hash to see if this index was caught when indexes didn't match
+                hint << "x" 
+                trackHash[y] = matchValue
+puts "values match but not indexes: hint: #{hint}, trackHash: #{trackHash}"
+              end
+            end # if indexes match
+
+          end # if values match
+puts ""
+        end # code each
+      end # guess each
+    guessCount = hint.join("") == "XXXX" ? guessCount : guessCount + 1 # either a correct guess or an entry has a digit > 7, either way count is paused
+    else # (for unless) detected a digit > 6 so guess not counted
+      hint = []
     end # unless
   end # if
 end # until
