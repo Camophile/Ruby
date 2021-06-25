abf05b816616da88f11d4a37af5ff0f73588f834
arman <kas1er@hotmail.com>
2021-06-24 18:29:06 -0700

    starting to get the hint out

--- Module1/problems/mastermind.rb
+++ Module1/problems/mastermind.rb
@@ -15,14 +15,27 @@ until hint == "XXXX"
         entry = gets.chomp
   if entry.size == 4 && entry.to_i > 0 # check if the string entered is 4 long and numerical/not negative
     guess = entry.split("").map(&:to_i)
+    puts "code: #{code}, guess: #{guess}"
+    puts ""
     unless guess.any? { | v | v >= 7 } # checks each element and if any >= 7 skips
-    code.each_with_index do | value, i |
-      if value == guess[i]
-        hint.prepend("X")
-      elsif code.include? guess[i]
-        hint.concat("x") # need to refactor this as it provide 'x'' for each guess of a value and doesn't take into consideration how many times a value occurs in the code
-      end
-    end # each
+      code.each_with_index do | codeValue, c |
+        guess.each_with_index do | guessValue, g |
+          if codeValue == guessValue
+            if c == g
+              hint.prepend("X")
+              break
+            elsif g > c # guess.any? { | v | code.include? v  } # code.include? guess[i]
+              puts "code index: #{c}, code value: #{codeValue}, guess index: #{g}, guess value: #{guessValue}"
+              hint.concat("x")
+              break
+          # puts "inside else - code value: #{value}, guess value: #{v}"
+            end # if
+# hint.concat("x") # need to refactor this as it provides 'x'' for each guess of a value and doesn't take into consideration how many times a value occurs in the "code"
+          else
+            
+          end # if
+        end # each
+      end # each
     guessCount = hint == "XXXX" ? guessCount : guessCount + 1 # either a correct guess or an entry has a digit > 7, either way count is paused
     else # detected a digit > 6 so guess not counted
       hint = ""
