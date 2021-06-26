aec32f353d4b29205a5ab2b73516d4c4087d71f7
arman <kas1er@hotmail.com>
2021-06-26 12:37:50 -0700

    the algorithm is working with puts commands for test

--- Module1/problems/mastermind.rb
+++ Module1/problems/mastermind.rb
@@ -1,19 +1,56 @@
-#code = Array.new(4) { rand(1..6) } 
+def hintAggregator (guess, code = Array.new(4) { rand(1..6) })
+  trackHash = {} # initiate track hash to see previous matches in the round
+  hint = []
+      
+    guess.each_with_index do | guessValue, i |
+      catch :captured_index do # we break out of loop so if a code index is captured we don't keep looking at the guesses (prevents duplicate hints when there is more than the same digit in the code)
+      code.each_with_index do | codeValue, y | # 
+puts "i: #{i}, y: #{y}, guessValue: #{guessValue}, codeValue: #{codeValue}, trackHash: #{trackHash}, hint: #{hint}"
+        if guessValue == codeValue
+          matchValue = guess[i]
+          if i == y
+puts "indexes match - trackHash[#{y}]: #{trackHash[y]}, matchValue: #{matchValue}, hint: #{hint}"
+            if trackHash[y] == matchValue # we look in the trackHash to see if this index was already caught
+              hint.pop if hint[-1] == "x" # if so we remove the previous non-index-matched hint and add the index one below
+puts "inside trackHash[#{y}] exists: hint: #{hint}"
+            end # if
+            hint.unshift("X") # place "X" at beginning when indexes match
+            trackHash[y] = matchValue
+              throw :captured_index # jump to the next index in the guess array
+puts "indexes values match after displace - trackHash[#{y}]: #{trackHash[y]}, matchValue: #{matchValue}, hint: #{hint}"
+          else # values match but not indexes
+puts "indexes values match but not indexes - trackHash[#{y}]: #{trackHash[y]}, matchValue: #{matchValue}"
+            unless trackHash[y] == matchValue # we look in the track hash to see if this index was caught when indexes didn't match
+              hint << "x" # if not already matched we add an "x"
+              trackHash[y] = matchValue
+puts "inside unless: trackHash[#{y}] does not equal match value - trackHash[#{y}]: #{trackHash[y]}, matchValue: #{matchValue}"
+             throw :captured_index # jumpt to the next indext in the guess array
+            end # unless
+          end # if indexes match
+        end # if values match
+puts ""
+      end # code each
+    end # guess each
+  end # end catch
+  hint
+end
+
 hint = []
-guessCount = 1
 
 puts "MASTERMIND"
 
 until hint.join("") == "XXXX"
+  guessCount = 1
 
         unless hint == [] # doesn't show hint if none of the digits match 
           puts hint.join("") 
         end
 
-        trackHash = {} # initiate track hash to see previous matches in the round
         hint = [] # reset the hint on each new attempt
 
-	code = [6, 4, 4 , 6]
+	#code = [1, 2, 3, 4]
+        code = [6, 4, 4, 6]
+
         print "[#{guessCount}] Enter your guess: "
         entry = gets.chomp
 
@@ -21,30 +58,10 @@ until hint.join("") == "XXXX"
     guess = entry.split("").map(&:to_i)
     unless guess.any? { | v | v >= 7 } # checks each element and if any >= 7 skips
 
-      guess.each_with_index do | guessValue, i |
-        code.each_with_index do | codeValue, y |
-          if guessValue == codeValue
-            matchValue = guess[i]
-            if i == y
-puts "indexes match"
-              if trackHash[y] == matchValue # we look in the trackHash to see if this index was already caught
-                hint.pop if hint[-1] == "x" # if so we remove the previous non-index-matched hint and add the index one below
-
-              end # if
-              hint.unshift("X") # place "X" at beginning when indexes match
-              trackHash[y] = matchValue
-            else # values match but not indexes
-              unless trackHash[y] == matchValue # we look in the track hash to see if this index was caught when indexes didn't match
-                hint << "x" # if not already matched we add an "x"
-              trackHash[y] = matchValue
-              end # unless
-            end # if indexes match
-
-          end # if values match
-        end # code each
-      end # guess each
+      hint = hintAggregator(guess, code) # code param passed for test otherwise instantiate above
+      
     guessCount = hint.join("") == "XXXX" ? guessCount : guessCount + 1 # either a correct guess or an entry has a digit > 7, either way count is paused
-    else # (for unless) detected a digit > 6 so guess not counted
+    else # detected a digit > 6 so guess not counted
       hint = []
     end # unless
   end # if
