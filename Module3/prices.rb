39f1ad3cc78e8566e0305607d75bf47dea3e08f9
arman <kas1er@hotmail.com>
2021-07-03 15:18:03 -0700

    added the 3 methods and answered the question prompts

--- Module3/prices.rb
+++ Module3/prices.rb
@@ -15,3 +15,7 @@ end
 puts total(prices)
 puts refund(prices)
 discount(prices).each { | price | puts format("Your discount: $%.2f", price) }
+
+# The first version (unrefactored) seems the simpler strategy since we're not mentally keeping track of which blocks of code to use and variables to put in play when and the logic is abstracted into each individual method definition. It's better to abstract the loops per method in this case rather than try to track which operation we should input into the reducer
+
+# Repitition for clarity if it doesn't weigh down or compromise performance and provides human legibility
