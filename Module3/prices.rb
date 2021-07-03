55d6bf68b95939f740b72b2bd524ec1c13aab01e
arman <kas1er@hotmail.com>
2021-07-03 15:04:35 -0700

    put each add, refund and discount prices into its own method and outputs the result of each operation

--- Module3/prices.rb
+++ Module3/prices.rb
@@ -1,2 +1,17 @@
-pricesArray = [3.99, 25.0, 8.99]
-puts format("%.2f", pricesArray.reduce(:+))
+prices = [3.99, 25.0, 8.99]
+
+def total(array)
+  format("%.2f", array.reduce(:+))
+end
+
+def refund(array)
+  format("%.2f", array.reduce(:-))
+end
+
+def discount(array)
+  discountedPrices = array.map { |i| i / 3.0}
+end
+
+puts total(prices)
+puts refund(prices)
+discount(prices).each { | price | puts format("Your discount: $%.2f", price) }
