171880d3e3919313ba25ce3e8e712d2f1e41bcec
arman <kas1er@hotmail.com>
2021-07-06 16:47:41 -0700

    Two ways of doing word search: each + delete_at & find_all

--- Module3/word_search.rb
+++ Module3/word_search.rb
@@ -1,6 +1,12 @@
 linesArray = []
 File.open("reviews.txt") { | review_file | linesArray = review_file.readlines  }
 
-puts "Line 4: #{linesArray[3]}"
-puts "Line 1: #{linesArray[0]}"
+linesArray = linesArray.find_all { | line | line.include?("Truncated") }
 
+#linesArray.each_with_index do | line, i |
+ # unless line.include?("Truncated")
+  #  linesArray.delete_at(i)
+  #end
+#end
+
+linesArray.each { |n| puts n}
