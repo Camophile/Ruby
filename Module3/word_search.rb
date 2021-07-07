0ab129411c3b5c8b0c7480bdcb492c69c3f966e1
arman <kas1er@hotmail.com>
2021-07-06 23:03:48 -0700

    breaks lines into array of words and loops through each word array to find the adj following is and adds to an adjectives array

--- Module3/word_search.rb
+++ Module3/word_search.rb
@@ -4,4 +4,10 @@ File.open("reviews.txt") { | review_file | linesArray = review_file.readlines  }
 linesArray = linesArray.find_all { | line | line if line.include?("Truncated") }
 linesArray = linesArray.reject { |l| l.include?("--") }
 
-linesArray.each { |n| puts n}
+adjectives = []
+linesArray.each do |l|
+  wordsArray = l.split(" ")
+  wordsArray.each_with_index { |w, i| adjectives << wordsArray[i+1] if w == "is" }
+end
+
+puts adjectives
