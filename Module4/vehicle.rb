287331ba0148007edc37e4dfa648682306d706a1
Arman Kazemi <arman.kazemi@shopify.com>
2021-07-09 17:57:56 -0700

    added an instance method to Truck class and gave the steer Vehicle method a variable so can reflect the type of vehicle doing steering

--- Module4/vehicle.rb
+++ Module4/vehicle.rb
@@ -6,8 +6,8 @@ class Vehicle
     puts "Floor it!"
   end
 
-  def steer
-    puts "Turn front 2 wheels."
+  def steer(wheels= "2 wheels")
+    puts "Turn front #{wheels}."
   end
 
   def mileage
@@ -17,17 +17,30 @@ end
 
 class Car < Vehicle
 end
+
 class Truck < Vehicle
+  attr_accessor :cargo
+
+  def load_bed(contents)
+    puts "Securing #{contents} in the truck."
+    @cargo = contents
+  end
 end
+
 class Motorcycle < Vehicle
 end
 
 truck = Truck.new
+truck.load_bed("259 bouncy balls")
+puts "The truck is carrying #{truck.cargo}"
 truck.accelerate
 truck.steer
 
 car = Car.new
 car.odometer = 11432
 car.gas_used = 366
-
 puts "Lifetime MPG:\n#{car.mileage}"
+
+motorcycle = Motorcycle.new
+motorcycle.steer("wheel")
+
