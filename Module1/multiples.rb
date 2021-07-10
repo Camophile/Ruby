691f96fa02e92d10c7cd447bd6eedb234ee1515c
Arman Kazemi <arman.kazemi@shopify.com>
2021-07-10 16:43:05 -0700

    added block versions of the customized list function to perform different logic on the entered multiplier and size

--- Module1/multiples.rb
+++ Module1/multiples.rb
@@ -1,11 +1,10 @@
 def customized_list_of_multiples(multiples_of, size)
   list = []
   size.times { | time | list << (time + 1) * multiples_of  }
-  
   if block_given?
-    yield(list)
+    puts "#{yield(list)}\n"
   else
-    list
+    puts "#{list}\n"
   end
 end
 
@@ -13,4 +12,13 @@ print "enter a number to create multiples of "
 multiplier = gets.chomp.to_i
 print "how many multiples of #{multiplier} do you want? "
 size = gets.chomp.to_i
-puts customized_list_of_multiples(multiplier, size)
+
+customized_list_of_multiples(multiplier, size) # returns array of multiples of multiplier to the length of size
+
+customized_list_of_multiples(multiplier, size) do | list | # for each multiple of multiplier, returns array of even ones
+  list.find_all { | num | num.even? }
+end
+
+customized_list_of_multiples(multiplier, size) do | list | # for each multiple of multiplier, returns array of multiple raised to the power of multiplier
+  list.map { | num | num ** multiplier }
+end
