ccf7deb47f33e607093d582b5729b1489cbfebca
Arman Kazemi <arman.kazemi@shopify.com>
2021-07-09 17:35:45 -0700

    created Vehicle superclass and created subclasses of vehicles from it and created instances of those subclasses

---
+++ Module4/vehicle.rb
@@ -0,0 +1,33 @@
+class Vehicle
+  attr_accessor :odometer
+  attr_accessor :gas_used
+
+  def accelerate
+    puts "Floor it!"
+  end
+
+  def steer
+    puts "Turn front 2 wheels."
+  end
+
+  def mileage
+    @odometer / @gas_used
+  end
+end
+
+class Car < Vehicle
+end
+class Truck < Vehicle
+end
+class Motorcycle < Vehicle
+end
+
+truck = Truck.new
+truck.accelerate
+truck.steer
+
+car = Car.new
+car.odometer = 11432
+car.gas_used = 366
+
+puts "Lifetime MPG:\n#{car.mileage}"
